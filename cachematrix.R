e8acdd1ca1c86dc070ee6dc77a35f832c3a470ff
unbelievableub <unbelievableub@gmail.com>
2014-10-17 15:31:37 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,22 +1,23 @@
 ## function A: create a "matrix" object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-elc <- NULL		##elc is NULL, we need this later to test whether something has been put in cache
+in_cache <- NULL		##in_chache is NULL, we need this later to test whether something has been put in cache
 
 ## define four functions....
 ## 1: setting the matrix
 set <- function(y) {
 x <<- y
-elc <<- NULL
+in_cache <<- NULL   ## ditto see above
 }
+
 ## 2: getting the matrix
 get <- function() x
 
-## 3: setting the inverse of the matrix
-setreverse<- function(reverse) elc <<-reverse
+## 3: setting the inverse of the matrix int cache
+setreverse<- function(reverse) in_cache <<-reverse
 
-## 4: getting the inverse of the matrix
-getreverse <- function() elc
+## 4: getting the inverse of the matrix from cache
+getreverse <- function() in_cache
 
 ## the 4 "descriptors"
 list(set = set, get = get,
@@ -28,17 +29,17 @@ getreverse = getreverse)
 cacheSolve <- function(x, ...) {
 
 ## Return a matrix that is the inverse of 'x'
-elc <- x$getreverse()
+in_cache <- x$getreverse()
 
 ## If the inverse has already been calculated, then retrieve the inverse from the cache.
-if (!is.null(elc)) {        
+if (!is.null(in_cache)) {        
 message("getting cached reververse matrix")
-return(elc)
+return(in_cache)
 
-## if it has not yet been calculated, calculate ("solve(x$et)") the inverse and store (setreverse) in elc
+## if it has not yet been calculated, calculate ("solve(x$et)") the inverse and store (setreverse)
 } else {
-elc <- solve(x$get())
-x$setreverse(elc)
-return(elc)
+in_cache <- solve(x$get())
+x$setreverse(in_cache)
+return(in_cache)
 }
 }
